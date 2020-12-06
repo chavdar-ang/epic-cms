acafe8402ad38773b7dab024c94ae209c19c599b
Chavdar Angelov <chavdar84@gmail.com>
2020-12-06 18:46:13 +0200

    Nested list refactoring. WIP.

--- src/database/schema/models/user.js
+++ src/database/schema/models/user.js
@@ -1,79 +1,90 @@
-export default {
-    settings: {
-        url: "https://jsonplaceholder.typicode.com/users",
-        name: "User",
-        slug: "user",
-        type: "document", // document or edge?
-        collection: "users",
-        uid: "_id",
-        description: "A description about the model.",
-        increments: true,
-        timestamps: true,
-    },
-    schema: {
-        name: 'name',
-        email: 'email',
-        username: 'username',
-        address: {
-            city: 'city',
-            street: 'street',
-            zipcode: "zipcode",
-            geo: {
-                lat: 'lat',
-                lng: 'lng'
-            }
-        },
-        phone: 'phone',
-        website: 'website'
-    },
-    list: {
-        name: 'name',
-        email: 'email',
-        username: 'username',
-        address: ':address.city :address.street'
-    },
-    fields: [
-        {
-            slug: "name",
-            name: "Name",
-            type: "group",
-            fields: [
-                {
-                    slug: "firstname",
-                    name: "First name",
-                    type: "input",
-                    style: "text",
-                    rules: {
-                        required: true,
-                        min: 3,
-                        max: 32
-                    }
-                },
-                {
-                    slug: "lastname",
-                    name: "Last name",
-                    type: "input",
-                    style: "text",
-                    rules: {
-                        required: true,
-                        min: 3,
-                        max: 32
-                    }
-                },
-            ]
-        },
-        {
-            slug: "email",
-            name: "Email",
-            type: "input",
-            style: "email",
-            rules: {
-                required: true,
-                email: true,
-                min: 8,
-                max: 64,
-                unique: true
-            }
+let settings = {
+    url: "https://jsonplaceholder.typicode.com/users",
+    name: "User",
+    slug: "user",
+    type: "document", // document or edge?
+    collection: "users",
+    uid: "_id",
+    description: "A description about the model.",
+    increments: true,
+    timestamps: true,
+};
+
+let schema = {
+    name: 'name',
+    email: 'email',
+    username: 'username',
+    address: {
+        city: 'city',
+        street: 'street',
+        zipcode: 'zipcode',
+        geo: {
+            lat: 'lat',
+            lng: 'lng'
         }
-    ]
+    },
+    phone: 'phone',
+    website: 'website'
+};
+
+let list = {
+    name: 'name',
+    email: 'email',
+    username: 'username',
+    address: ':address.city and :address.street'
+};
+
+let fields = {
+    name: {
+        name: "Name",
+        type: "input",
+        style: "text",
+        rules: {
+            required: true,
+            min: 3,
+            max: 32
+        }
+    },
+    email: {
+        name: "Email",
+        type: "input",
+        style: "email",
+        rules: {
+            required: true,
+            email: true,
+            min: 8,
+            max: 64,
+            unique: true
+        }
+    },
+    address: {
+        name: "Address",
+    },
+    city: {
+        name: "City",
+        type: "input",
+        style: "text",
+        rules: {
+            required: true,
+            min: 8,
+            max: 64,
+        }
+    },
+    street: {
+        name: "Street",
+        type: "input",
+        style: "text",
+        rules: {
+            required: true,
+            min: 8,
+            max: 64,
+        }
+    }
+}
+
+export default {
+    settings: settings,
+    schema: schema,
+    list: list,
+    fields: fields
 }
--- src/pages/models/[slug]/_components/Row.svelte
+++ src/pages/models/[slug]/_components/Row.svelte
@@ -1,15 +1,65 @@
 <script>
-  import _ from 'lodash';
+  import _ from "lodash";
+  import schema from "../../../../database/schema";
   export let item, model;
 
   // check user list -> :address.city :address.street
 
-  // render list function
+  //  console.log('flat', Object.keys(model.list).flat(2));
+
+  // render schema function
+
+  let list = model.list;
+
+  let column = field => {
+    if (list[field].indexOf(":") === -1) return item[field];
+
+    const regex = /:[a-z.]+/g;
+    let matches = list[field].match(regex);
+
+    // console.log("field", list[field]);
+
+    
+
+    // test.replace(":address.street", "test");
+
+    // console.log('res ', res);
+    let test = list[field];
+
+    // test.replace(":", "1");
+
+    // console.log('zzz', test.replace(":", "1"));
+
+
+    matches.map(m => {
+      console.log("m:", m);
+      // return `${_.get()}`
+      // return _.get(item, [m]);
+
+      // console.log('zxc', field);
+      // let rep = `:${m}`;
+
+      // console.log('res:', res, 'replaced:', res.replace(`:${m}`, "test"));
+
+      
+      test = test.replace(m, "test");
+      return test;
+      // console.log('test1', test);
+      // res.replace(m, "test");
+    });
+    console.log("full", test);
+
+    // return test;
+
+    // console.log('matches', matches);
+
+    return "nothing";
+  };
 </script>
 
 <tr>
-  {#each Object.keys(model.list) as field}
-    <td>{item[field]}</td>
+  {#each Object.keys(list) as field}
+    <td>{column(field)}</td>
   {/each}
   <td>
     <a href="/models/{model.settings.slug}/update/{item['_id']}"> Edit</a>
